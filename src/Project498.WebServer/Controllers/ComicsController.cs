46559793d3a421df12beeccced62b4aa11fc6931
Alexandra Fomina <alexandrafomina@Alexandras-MacBook-Air-8.local>
2026-03-19 15:02:26 -0700

    Built functionality for top search bar

---
+++ src/Project498.WebServer/Controllers/ComicsController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Project498.WebServer.Services;
+
+namespace Project498.WebServer.Controllers;
+
+public class ComicsController : Controller
+{
+    private readonly MockComicService _mockComicService;
+
+    public ComicsController(MockComicService mockComicService)
+    {
+        _mockComicService = mockComicService;
+    }
+
+    public IActionResult Details(int id)
+    {
+        var comic = _mockComicService.GetById(id);
+
+        if (comic == null)
+        {
+            return RedirectToAction("Index", "Explore");
+        }
+
+        ViewBag.RelatedComics = _mockComicService
+            .GetAll()
+            .Where(c => c.Id != comic.Id &&
+                        (c.Genre == comic.Genre || c.SecondaryGenre == comic.SecondaryGenre))
+            .Take(3)
+            .ToList();
+
+        return View(comic);
+    }
+}
